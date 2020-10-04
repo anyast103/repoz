784eda73c6ea2456fd3315deae5c69408e198b92
anyast103 <70793585+anyast103@users.noreply.github.com>
2020-10-04 11:33:37 +0300

    Add files via upload

---
+++ Calc/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------------------------------");
+            Console.Write("2 + 3" + "->");
+            Console.Write(GetCalc("2 + 3"));
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------");
+            Console.Write("2          +      3" + "->");
+            Console.Write(GetCalc("2          +      3"));
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------");
+            Console.Write("1 / 2" + "->");
+            Console.Write(GetCalc("1 / 2"));
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------");
+            Console.Write("1 / 0" + "->");
+            Console.Write(GetCalc("1 / 0"));
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------");
+            Console.Write("1 ! 0" + "->");
+            Console.Write(GetCalc("1 ! 0"));
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------");
+        }
+
+        static string GetCalc(string text)
+        {
+            var parseText = text
+                .Split()
+                .Where(x => x != "")
+                .ToArray();
+            var firstNumber = double.Parse(parseText[0]);
+            var secondNumber = double.Parse(parseText[2]);
+            switch (parseText[1])
+            {
+                case "+":
+                    return (firstNumber + secondNumber).ToString();
+                case "-":
+                    return (firstNumber - secondNumber).ToString();
+                case "*":
+                    return (firstNumber * secondNumber).ToString();
+                case "/":
+                    if (secondNumber != 0)
+                        return (firstNumber / secondNumber).ToString();
+                    else return "Недопустимая операция!";
+                default:
+                    return "Недопустимая операция!";
+
+            }
+
+
+        }
+    }
+}
